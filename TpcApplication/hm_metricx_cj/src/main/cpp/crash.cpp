e68a6c8fde3bdd41bc06422515d0481f7458a9b0
naigogian <i6122f@163.com>
2025-04-02 11:53:10 +0800

    initNativeSignalHandler

---
+++ TpcApplication/hm_metricx_cj/src/main/cpp/crash.cpp
@@ -0,0 +1,108 @@
+//
+// Created on 2025/4/1.
+//
+// Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
+// please include "napi/native_api.h".
+
+#include "crash.h"
+#include <signal.h>
+#include <malloc.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/eventfd.h>
+#include <unistd.h>
+#include <syscall.h>
+#include <stdlib.h>
+
+typedef struct {
+    int sigNum;
+    struct sigaction oldact;
+} SignalCrashInfo;
+
+static pthread_mutex_t signalHandlerMutex = PTHREAD_MUTEX_INITIALIZER;
+
+static sig_atomic_t inCrash = 0;
+
+int initSuccess = 0;
+
+typedef const char* (*CollectCrashInfo)();
+
+CollectCrashInfo cjCollectCrashInfo;
+
+typedef void (*Callback)(const char*, const char*, CollectCrashInfo);
+
+Callback cjcb;
+
+const char* persistentFilePath;
+
+const char* cjLimits;
+
+static SignalCrashInfo signalCrashInfo[] =
+        {
+                {.sigNum = SIGABRT},
+                {.sigNum = SIGBUS},
+                {.sigNum = SIGFPE},
+                {.sigNum = SIGILL},
+                {.sigNum = SIGSEGV},
+                {.sigNum = SIGTRAP},
+                {.sigNum = SIGSYS},
+                {.sigNum = SIGSTKFLT}
+        };
+
+int RemoveSignalHandler()
+{
+    int r = SUCCESS;
+    size_t i;
+    for (i = 0; i < sizeof(signalCrashInfo) / sizeof(signalCrashInfo[0]); i++) {
+        if (0 != sigaction(signalCrashInfo[i].sigNum, &(signalCrashInfo[i].oldact), NULL)) {
+            r = FAIL;
+        }
+    }
+    return r;
+}
+
+static void CrashSignalHandler(int sig, siginfo_t *si, void *context)
+{
+    if (initSuccess == 0) {
+        signalCrashInfo[sig].oldact.sa_sigaction(sig, si, context);
+        return;
+    }
+    
+    pthread_mutex_lock(&signalHandlerMutex);
+    if (inCrash) {
+        pthread_mutex_unlock(&signalHandlerMutex);
+        signalCrashInfo[sig].oldact.sa_sigaction(sig, si, context);
+        return;
+    }
+
+    inCrash = 1;
+    cjcb(persistentFilePath, cjLimits, cjCollectCrashInfo);
+    RemoveSignalHandler();
+    pthread_mutex_unlock(&signalHandlerMutex);
+    signalCrashInfo[sig].oldact.sa_sigaction(sig, si, context);
+}
+
+extern "C" {
+int8_t InitNativeSignalHandler(const char* pFilePath, const char* limits, CollectCrashInfo collectCrashInfo, Callback cb)
+{
+    struct sigaction act;
+    memset(&act, 0, sizeof(act));
+    sigfillset(&act.sa_mask);
+    act.sa_sigaction = CrashSignalHandler;
+    act.sa_flags = SA_RESTART | SA_SIGINFO | SA_ONSTACK;
+
+    size_t i;
+    for (i = 0; i < sizeof(signalCrashInfo) / sizeof(signalCrashInfo[0]); i++) {
+        if (0 != sigaction(signalCrashInfo[i].sigNum, &act, &(signalCrashInfo[i].oldact))) {
+            RemoveSignalHandler();
+            return FAIL;
+        }
+    }
+    initSuccess = 1;
+    persistentFilePath = pFilePath;
+    cjLimits = limits;
+    cjCollectCrashInfo = collectCrashInfo;
+    cjcb = cb;
+    return SUCCESS;
+}
+}
---
+++ TpcApplication/hm_metricx_cj/src/main/cpp/crash.h
@@ -0,0 +1,13 @@
+//
+// Created on 2025/4/1.
+//
+// Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
+// please include "napi/native_api.h".
+
+#ifndef TPCAPPLICATION_CRASH_H
+#define TPCAPPLICATION_CRASH_H
+
+#define SUCCESS (0)
+#define FAIL (-1)
+
+#endif //TPCAPPLICATION_CRASH_H
