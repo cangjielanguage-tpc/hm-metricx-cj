21248d133afea48cf97e37ce85f1c13fb6abe785
weinw <weinw@noreply.gitcode.com>
2025-09-05 11:38:24 +0800

    remove useless lock
    x

--- TpcApplication/hm_metricx_cj/src/main/cpp/common.cpp
+++ TpcApplication/hm_metricx_cj/src/main/cpp/common.cpp
@@ -5,23 +5,19 @@
 // please include "napi/native_api.h".
 
 #include "common.h"
-#include <mutex>
 #include <vector>
 
 bool isLogCallbackRegistered = false;
 std::vector<LogCallback> logCallbacks;
-std::mutex logCallbackMutex;
 
 void HilogCallback(const LogType type, const LogLevel level, const unsigned int domain, const char *tag,
                    const char *msg) {
-    std::lock_guard<std::mutex> lock(logCallbackMutex);
     for (const auto &callback : logCallbacks) {
         callback(type, level, domain, tag, msg);
     }
 }
 
 void registerHilogCallback(LogCallback logCallback) {
-    std::lock_guard<std::mutex> lock(logCallbackMutex);
     logCallbacks.push_back(logCallback);
     if (isLogCallbackRegistered) {
         return;
