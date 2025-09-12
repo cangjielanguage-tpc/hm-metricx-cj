b85a8664ef7b286976647ae2dc0e2dfda027eb19
weinw <weinw@noreply.gitcode.com>
2025-09-12 15:50:31 +0800

    add url traffic

---
+++ TpcApplication/hm_metricx_cj/src/main/cpp/traffic/hook.h
@@ -0,0 +1,12 @@
+//
+// Created on 2025/9/4.
+//
+// Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
+// please include "napi/native_api.h".
+
+#ifndef TPCAPPLICATION_TRAFFIC_HOOK_H
+#define TPCAPPLICATION_TRAFFIC_HOOK_H
+
+int hookSocket();
+
+#endif //TPCAPPLICATION_TRAFFIC_HOOK_H
