eacbf6b5581eb55210ff853a58c44e3f3bdc5f59
weinw <weinw@noreply.gitcode.com>
2025-09-28 17:10:08 +0800

    remove useless assets in har

--- ApmApplication/hvigorfile.ts
+++ ApmApplication/hvigorfile.ts
@@ -1,6 +1,39 @@
 import { appTasks } from '@ohos/hvigor-ohos-plugin';
+import { hvigor } from '@ohos/hvigor';
+import * as fs from 'fs';
+
+hvigor.nodesEvaluated(() => {
+  const root = hvigor.getRootNode();
+  root.subNodes(node => {
+    if (node.getNodeName() === 'hm_metricx_cj') {
+      const task = node.getTaskByName('default@CacheNativeLibs');
+      task.afterRun(() => {
+        const libsDir = node.getNodePath() + '/build/default/intermediates/stripped_native_libs/default/arm64-v8a';
+        if (fs.existsSync(libsDir + '/libc++_shared.so')) {
+          fs.rmSync(libsDir + '/libc++_shared.so');
+        }
+        const cjbins = libsDir + '/cjbins/ohos_app_cangjie_hm_metricx_cj';
+        for (const entry of fs.readdirSync(cjbins)) {
+          const fullpath = cjbins + '/' + entry;
+          if (entry.endsWith('.cjo')) {
+            fs.rmSync(fullpath);
+          }
+        }
+        const ohosDir = libsDir + '/ohos';
+        for (const entry of fs.readdirSync(ohosDir)) {
+          const fullpath = ohosDir + '/' + entry;
+          if (entry !== 'libark_interop_diff.z.so' && entry !== 'libcangjie-runtime_diff.z.so') {
+            fs.rmSync(fullpath);
+          }
+        }
+      });
+    }
+  });
+});
+
 
 export default {
   system: appTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
   plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
 }
+
